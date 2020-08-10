f5358135fface99e545209fc2236f99a18d74d26
Sakeef Sayeed <sakeefsayeed@gmail.com>
2020-08-09 18:07:02 -0700

    Solution to REVC

---
+++ src/REVC.java
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
+public class REVC {
+
+    private static StringBuilder reverse (String initial){
+        StringBuilder reversed = new StringBuilder(initial);
+        reversed = reversed.reverse();
+        return reversed;
+    }
+
+    private static StringBuilder compliment (StringBuilder reversed){
+        StringBuilder complimented = new StringBuilder();
+        for (int i = 0; i < reversed.length(); i++){
+            if (reversed.charAt(i) == 'A'){
+                complimented.append('T');
+            } else if (reversed.charAt(i) == 'T'){
+                complimented.append('A');
+            } else if (reversed.charAt(i) == 'C'){
+                complimented.append('G');
+            } else if (reversed.charAt(i) == 'G'){
+                complimented.append('C');
+            }
+        }
+        return complimented;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File dataset = new File("rosalind_revc.txt");
+        Scanner scanner = new Scanner(dataset);
+        while (scanner.hasNextLine()){
+            String data = scanner.nextLine();
+            StringBuilder reversed = reverse(data);
+            StringBuilder complimented = compliment(reversed);
+            System.out.println(complimented);
+        }
+    }
+}
